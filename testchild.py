1fc1a0ff448a0db9a16a81763f4cea12d5363ab5
ugwu-rex <soromtochukwu.ugwu.258465@unn.edu.ng>
2026-03-25 20:02:21 +0100

    Added a new feature

--- testchild.py
+++ testchild.py
@@ -1,4 +1,21 @@
 # Display welcome message
-message = "Hello "
+message = "Hello"
+
 name = input("Enter your name! ")
-print (message, name.title())
+
+name = name.title()
+
+print (message, name)
+
+
+print("For the Party there\'s an age restrictions...")
+
+age = int(input("what is your age? "))
+
+if (age >= 18):
+    print ( "welcome, " + name)
+elif age >= 16 and age < 18:
+    print(name + " You are not yet eligible, you still have a little way to go.")
+else:
+    print("You're too young " + name)
+
